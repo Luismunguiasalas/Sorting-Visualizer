2c66328f14689a40b9ac0300f51b1dc9ede6ce37
Luis Munguia Salas <36023613+Luismunguiasalas@users.noreply.github.com>
2023-02-11 09:51:37 -0800

    working on bubble sort, added ID's to html, created bubble sort algorithm
    finished creating event handler, and bubble sort function. lets test it out

--- algorithms.js
+++ algorithms.js
@@ -1,39 +1,83 @@
-// get main element
-const main = document.getElementById('main');
-const generate_array = document.getElementById('generate-array')
-// create element
-const div = document.createElement('div');
-// to hold main element children elements
-let array_children = null;
 
-// generate array
+const mainEl = document.getElementById('main'); // get main element
+const bubbleSortEl = document.getElementById('bubble-sort'); // get bubble sort element
+const insertionSortEl = document.getElementById('insertion-sort'); // get insertion sort element
+const quickSortEl = document.getElementById('quick-sort'); // get quick sort element
+const mergeSortEl = document.getElementById('merge-sort'); // get merge sort element
+const generateArrayEl = document.getElementById('generate-array') // create div element
+let array_children = null; // to hold array of div elements
+
+/**
+ * generateNewArray(): 
+ * will create 257 div elements and assign then a random height
+ * the div elements will be appended to the 'mainEl'
+ */
 function generateNewArray() {
   for (let i = 0; i < 257; i++) {
     const div = document.createElement('div');
-    const divHeight = getRandomInteger(10, 650);
-    div.style.height = divHeight.toString() + 'px';
-    main.appendChild(div);
+    const divHeight = getRandomInteger(10, 650); // assing random (height) number 
+    div.style.height = divHeight.toString() + 'px'; // add height to div
+    mainEl.appendChild(div);
   }
 }
-
-// random number generator
+/**
+ * 
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns 
+ */
+// random number generator helper function for 'generateNewArray'
 function getRandomInteger(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-// generate new array when button is clicked, but also removes any existing child div elements
-generate_array.onclick = () => {
-  while (main.firstChild) {
-    main.removeChild(main.lastChild);
+/**
+ * Event handler:
+ * generate new array when button 'generate array' is clicked, also, removes any existing child div elements
+ */
+generateArrayEl.onclick = () => {
+  while (mainEl.firstChild) {
+    mainEl.removeChild(mainEl.lastChild);
   }
   generateNewArray();
-  array_children = main.children;
+  array_children = mainEl.children;
+}
+
+/** BUBBLESORT */
+
+/**
+ * bubbleSort():
+ * traverse through all array elements
+ * swap if the element foud is grater than the next element
+ * @param {Array} unsortedArray 
+ */
+function bubbleSort(unsortedArray) {
+  const n = unsortedArray.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < (n - i - 1); j++) {
+      if (unsortedArray[j] > unsortedArray[j + 1]) {
+        let height = unsortedArray[j].divHeight;
+        let height2 = unsortedArray[j + 1].divHeight;
+        console.log(height);
+        console.log(height2);
+        unsortedArray[j], unsortedArray[j + 1] = unsortedArray[j + 1], unsortedArray[j]
+      }
+    }
+  }
+}
+
+/**
+ * Event handler:
+ */
+
+bubbleSortEl.onclick = () => {
+  bubbleSort(unsortedArray);
 }
 
 
 
 
-
+/** MERGE SORT SECTION, WORRY ABOUT THIS LATER */
 // the merge sort function
 // Merge Sort Implentation (Recursion)
 function mergeSort(unsortedArray) {
